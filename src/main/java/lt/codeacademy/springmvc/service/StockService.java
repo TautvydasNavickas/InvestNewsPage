9a4d89cd080389d391250892c65d80ad3480ffd5
Tautvydas Navickas <taut.navickas@gmail.com>
2022-05-30 02:43:01 +0300

    stockServiceTest was updated to test by finging multiple stocks

--- src/main/java/lt/codeacademy/springmvc/service/StockService.java
+++ src/main/java/lt/codeacademy/springmvc/service/StockService.java
@@ -8,11 +8,15 @@ import yahoofinance.YahooFinance;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Service
 public class StockService {
 
+        private final RefreshService refreshService;
     public StockWrapper findStock(String ticker) {
 
 
@@ -25,8 +29,12 @@ public class StockService {
         return null;
     }
 
+    public List<StockWrapper> findStocks(List<String> tickers){
+        return tickers.stream().map(this::findStock).filter(Objects::nonNull).collect(Collectors.toList());
+    }
+
     public BigDecimal findPrice (StockWrapper stockWrapper) throws  IOException{
-        return stockWrapper.getStock().getQuote(true).getPrice();
+        return stockWrapper.getStock().getQuote(refreshService.refresher(stockWrapper)).getPrice();
     }
 
 
--- src/test/java/lt/codeacademy/springmvc/service/stockServiceTest.java
+++ src/test/java/lt/codeacademy/springmvc/service/stockServiceTest.java
@@ -9,6 +9,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
 
 @Slf4j
 @SpringBootTest
@@ -26,5 +28,19 @@ import java.math.BigDecimal;
 
         final BigDecimal price = stockService.findPrice(stockWrapper);
         System.out.println(price);
+    }
+
+    @Test
+    void multiple() throws  IOException, InterruptedException{
+        final List<StockWrapper> stocks = stockService.findStocks(Arrays.asList("GOOG", "AMZM"));
+        stockService.findPrice((StockWrapper) stocks);
+
+        Thread.sleep(16000);
+
+        final StockWrapper stockWrapper = stockService.findStock("AA.L");
+        stocks.add(stockWrapper);
+
+        System.out.println(stockService.findPrice(stockWrapper));
+
     }
 }
