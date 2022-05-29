69a9738d168fa4cee05142ccb82a030abac2bf6c
Tautvydas Navickas <taut.navickas@gmail.com>
2022-05-29 17:33:16 +0300

    Mail template and utility package was added

---
+++ src/main/java/lt/codeacademy/springmvc/utility/Constant.java
@@ -0,0 +1,9 @@
+package lt.codeacademy.springmvc.utility;
+
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Constant {
+    public static final String ACTIVATION_EMAIL = "http://localhost:8080/api/auth/accountVerification";
+}
