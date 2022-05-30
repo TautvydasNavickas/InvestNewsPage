2eef96514f04c293e3729ca6fee6dd78d67d832c
Tautvydas Navickas <taut.navickas@gmail.com>
2022-05-31 01:21:43 +0300

    Updated

--- src/main/java/lt/codeacademy/springmvc/repository/entity/EmailNotification.java
+++ src/main/java/lt/codeacademy/springmvc/repository/entity/EmailNotification.java
@@ -7,4 +7,8 @@ public class EmailNotification {
     private String subject;
     private String recipient;
     private String body;
+
+    public EmailNotification(String please_activate_your_account, String email, String message) {
+
+    }
 }
--- src/main/java/lt/codeacademy/springmvc/service/AuthorizationService.java
+++ src/main/java/lt/codeacademy/springmvc/service/AuthorizationService.java
@@ -2,13 +2,20 @@ package lt.codeacademy.springmvc.service;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import lt.codeacademy.springmvc.DTO.AuthResponse;
+import lt.codeacademy.springmvc.DTO.LoginRequest;
 import lt.codeacademy.springmvc.DTO.RegisterRequest;
+import lt.codeacademy.springmvc.configuration.JwtProvider;
 import lt.codeacademy.springmvc.exception.InvestAccountNotFoundException;
 import lt.codeacademy.springmvc.repository.UserRepository;
 import lt.codeacademy.springmvc.repository.VerificationRepository;
+import lt.codeacademy.springmvc.repository.entity.EmailNotification;
 import lt.codeacademy.springmvc.repository.entity.User;
 import lt.codeacademy.springmvc.repository.entity.Verification;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -18,22 +25,20 @@ import java.util.Optional;
 import java.util.UUID;
 
 import static java.time.Instant.now;
+import static lt.codeacademy.springmvc.utility.Constant.ACTIVATION_EMAIL;
 
 @Service
 @AllArgsConstructor
 @Slf4j
 public class AuthorizationService {
 
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final VerificationRepository verificationRepository;
-    private final MailService mailService;
-
-    public AuthorizationService(UserRepository userRepository, PasswordEncoder passwordEncoder, VerificationRepository verificationRepository) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-        this.verificationRepository = verificationRepository;
-    }
+    private  UserRepository userRepository;
+    private  PasswordEncoder passwordEncoder;
+    private  VerificationRepository verificationRepository;
+    private  SendingMailService sendingMailService;
+    private  AuthenticationManager authenticationManager;
+    private  MailMessageContentService mailMessageContentService;
+    private JwtProvider jwtProvider;
 
     @Transactional
     public void signUp(RegisterRequest registerRequest) {
@@ -46,7 +51,12 @@ public class AuthorizationService {
 
         userRepository.save(user);
 
+        log.info("User Registered Successfully, Sending Authentication Email");
         String token = generateVerificationToken(user);
+        String message = mailMessageContentService.build("Thank you for signing up to Spring Reddit, please click on the below url to activate your account : "
+                + ACTIVATION_EMAIL + "/" + token);
+
+        sendingMailService.sendMail(new EmailNotification("Please Activate your account", user.getEmail(), message));
     }
 
     private String generateVerificationToken(User user) {
@@ -60,6 +70,14 @@ public class AuthorizationService {
 
     private String encodePassword(String password) {
         return passwordEncoder.encode(password);
+    }
+
+    public AuthResponse login(LoginRequest loginRequest) {
+        Authentication authenticate = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(),
+                loginRequest.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authenticate);
+        String authenticationToken = jwtProvider.generateToken(authenticate);
+        return new AuthResponse(authenticationToken, loginRequest.getUsername());
     }
 
     public void verifyAccount(String token) {
